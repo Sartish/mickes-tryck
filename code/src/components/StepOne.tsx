63b304c40c32fef48e6b67672840b2fcb5a595a5
Sara Carlstein <carlstein.sara@gmail.com>
2021-10-19 14:50:32 +0200

    added form functions

--- code/src/components/StepOne.tsx
+++ code/src/components/StepOne.tsx
@@ -6,7 +6,7 @@ import Form from './Form';
 import Input from './Input';
 import FormButton from './FormButton';
 import { useHistory } from 'react-router-dom';
-import { Typography } from '@material-ui/core';
+import { TextField } from '@material-ui/core';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { FileInput } from './FileInput';
 import * as yup from 'yup';
@@ -17,64 +17,46 @@ type Profile = {
   email: string;
 };
 
-type values = {
-  setValues: string;
-  data: string;
-};
-
 const schema = yup.object().shape({
-  firstName: yup
-    .string()
-    .matches(/^([^0-9]*)$/, 'First name should not contain numbers')
-    .required('First name is a required field'),
-  lastName: yup
-    .string()
-    .matches(/^([^0-9]*)$/, 'Last name should not contain numbers')
-    .required('Last name is a required field'),
+  firstname: yup.string().required('Firstname is required'),
+  lastname: yup.string().required('Lastname is required'),
+  email: yup.string().email('Must be a valid email').max(255).required('Email is required'),
 });
 
 const StepOne = () => {
-  const [setValues, data] = useState<values>({
-    setValues: '',
-    data: '',
-  });
-  const history = useHistory();
+  // const history = useHistory();
   const { register, handleSubmit, errors, control } = useForm({
-    defaultValues: { firstName: data, lastName: data, email: data, files: data },
     mode: 'onBlur',
     resolver: yupResolver(schema),
   });
 
   // data received in type of profile
   const onSubmit = (data: Profile) => {
-    history.push('./step2');
-    // @ts-ignore
-    // setValues(data);
+    console.log(data);
   };
 
   return (
     <MainContainer>
-      {/* <Typography component="h2" variant="h5">
-        🦄 Step 1
-      </Typography> */}
       <Form onSubmit={handleSubmit(onSubmit)}>
         <Input
           ref={register}
-          id="firstName"
+          id="firstname"
           type="text"
           label="First Name"
-          name="firstName"
-          error={!!errors.firstName}
-          // helperText={errors?.firstName?.message}
+          name="firstname"
+          error={!!errors.firstname}
+          helperText={errors?.firstname?.message}
+          required
         />
         <Input
           ref={register}
-          id="lastName"
+          id="lastname"
           type="text"
           label="Last Name"
-          name="lastName"
-          error={!!errors.lastName}
-          // helperText={errors?.lastName?.message}
+          name="lastname"
+          error={!!errors.lastname}
+          helperText={errors?.lastname?.message}
+          required
         />
         <Input
           ref={register}
@@ -83,10 +65,18 @@ const StepOne = () => {
           label="Email"
           name="email"
           error={!!errors.email}
-          // helperText={errors?.email?.message}
+          helperText={errors?.email?.message}
           required
         />
         <FileInput name="files" control={control} />
+        <TextField
+          id="outlined-multiline-static"
+          label="messege"
+          multiline
+          rows={4}
+          defaultValue="other info you want to add?"
+          fullWidth
+        />
         <FormButton>Next</FormButton>
       </Form>
     </MainContainer>
